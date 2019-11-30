ff967ddb620268597e247b32b48e309033c1919c
Abhijeet Singh <redefinedabhi2@gmail.com>
2019-12-01 02:01:50 +0530

    DetailActivity Created DetailActivity Layout is designed

---
+++ app/src/main/java/com/udacity/popularmoviesstage1app/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.udacity.popularmoviesstage1app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.popularmoviesstage1app.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
